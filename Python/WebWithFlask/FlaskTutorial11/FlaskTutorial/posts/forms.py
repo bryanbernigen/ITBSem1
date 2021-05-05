ef10221599f625058cebb706233798010ba3c945
Bryan Bernigen <bryanbernigen@gmail.com>
2021-05-05 11:48:03 +0700

    Added Flask Tutorial Submodule

--- Python/WebWithFlask/FlaskTutorial11/FlaskTutorial/posts/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-class PostForm(FlaskForm):
-    title = StringField('Judul', validators=[DataRequired()])
-    content = TextAreaField('Konten', validators=[DataRequired()])
-    submit = SubmitField('Unggah')
--- Python/WebWithFlask/FlaskTutorial5/run.py
+++
@@ -1,4 +0,0 @@
-from Flask_Tutorial import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
